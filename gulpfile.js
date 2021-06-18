1aff7367ade231dcd32c3c5a47176d2c12530c22
Emanouil <e_alb|@abv.bg>
2021-06-18 20:33:50 +0300

    Some message

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
